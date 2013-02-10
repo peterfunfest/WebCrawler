55fe2bf01b13177a2c271efe8c092c7365513da4
IAINLAPTOP <IAINLAPTOP@IAINLAPTOP-PC.lan>
2013-02-10 22:04:47 +0000

    Test

--- WebCrawler/src/junk/JunkClass.java
+++ WebCrawler/src/junk/JunkClass.java
@@ -1,9 +1,20 @@
 package junk;
 
+/**
+ * 
+ * @author IAINLAPTOP
+ *
+ */
 public class JunkClass {
 	
 	// Dummy class to test Git commit 
 	
 	// Additional Comment
 
+	/**
+	 * 
+	 */
+	public void methodTest(){
+		
+	}
 }
